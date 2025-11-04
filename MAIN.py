c395abe2dc31dce7bf956cea305071bda6881325
Jack Armstrong <jackarmstrong819@gmail.com>
2025-11-04 13:24:26 +0000

    adding structure

---
+++ MAIN.py
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+start = True
+
+while start is True:
+
+    print("1.View Stock \n2.Add Item \n3.Update Item \n4.Search \nq for Quit ")
+    choice = input("PLease enter the option you want")
+
+    if choice == "1":
+        print("you have chose to view stock")
+
+    elif choice == "2":
+        print("you have chosen to Add item")
+
+    elif choice == "3":
+        print("you have chosen to Update item")
+
+    elif choice == "4":
+        print("you have chosen to Search")
+
+    elif choice == "q":
+        print("you have chosen to Quit")
+        start = False
+
+    else:
+        print("please enter a correct value")
