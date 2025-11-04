1ffc616b82cb8e82c1f2f96be56c8b4c81be8562
Jack Armstrong <jackarmstrong819@gmail.com>
2025-11-04 13:31:41 +0000

    adding more structure

--- MAIN.py
+++ MAIN.py
@@ -4,28 +4,32 @@
 
 
 
-start = True
+def main():
 
-while start is True:
+    while True:
 
-    print("1.View Stock \n2.Add Item \n3.Update Item \n4.Search \nq for Quit ")
-    choice = input("PLease enter the option you want")
+        print("1.View Stock \n2.Add Item \n3.Update Item \n4.Search \nq for Quit ")
+        choice = input("PLease enter the option you want")
 
-    if choice == "1":
-        print("you have chose to view stock")
+        if choice == "1":
+            print("you have chose to view stock")
 
-    elif choice == "2":
-        print("you have chosen to Add item")
+        elif choice == "2":
+            print("you have chosen to Add item")
 
-    elif choice == "3":
-        print("you have chosen to Update item")
+        elif choice == "3":
+            print("you have chosen to Update item")
 
-    elif choice == "4":
-        print("you have chosen to Search")
+        elif choice == "4":
+            print("you have chosen to Search")
 
-    elif choice == "q":
-        print("you have chosen to Quit")
-        start = False
+        elif choice == "q":
+            print("you have chosen to Quit")
+            break
 
-    else:
-        print("please enter a correct value")
+        else:
+            print("please enter a correct value")
+
+
+if __name__ == "__main__":
+    main()
