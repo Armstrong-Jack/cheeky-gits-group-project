7dcf814a2cc173b575116cafcd3a142f47287ce2
Jack-Armstrong <jackarmstrong819@gmail.com>
2025-11-18 15:53:13 +0000

    f

--- class_test.py
+++ class_test.py
@@ -1,9 +1,12 @@
 class product:
     def __init__(self, ID,):
-        self.product_data = {"26628503": {"name": "Fanta", "price" : 1.5}}
+        self.product_data = { "26628503": {"name":"Fanta", "price" :2.70}}
         self.ID = ID
         self.name = self.product_data[ID]["name"]
         self.price = self.product_data[ID]["price"]
+
+
+
 
     def order(self, amount):
 
@@ -17,36 +20,51 @@ class product:
 
 #print(product_data["26628503"]["name"])
 
+prod = product()
+IDs = prod.product_data
+if IDs == "NULL":
+    print("their are no IDs/Products")
+    prod = product()
+    choice = input("would you like to add product ID")
+    if choice == "y":
+        ne = input("enter product name")
+        id = input("product_id")
+        pr = int(input("add the products price"))
 
-IDs = ["26628503", "26628504", "26628505", "26628506", "26628507", "26618501", "26618502", "26618503", "26618504", "26618505"]
-entered_ID = input("Enter the ID to search: ")
-ID_search_length = len(entered_ID)
-familler_ID =[]
+        prod.add_product(id, ne, pr)
+        
+        print("succsesful here is the new list of products")
+        print(prod.product_data)
+   
+else:
+    entered_ID = input("Enter the ID to search: ")
+    ID_search_length = len(entered_ID)
+    familler_ID =[]
 
-for ID in IDs:
-    ID2 = ID[0:ID_search_length]
-    if entered_ID == ID2:
-        familler_ID.append(ID)
-        print(familler_ID)
+    for ID in IDs:
+        ID2 = ID[0:ID_search_length]
+        if entered_ID == ID2:
+            familler_ID.append(ID)
+            print(familler_ID)
 
-for ID in familler_ID:
-    print(f"{familler_ID.index(ID)}. {ID}")
+    for ID in familler_ID:
+        print(f"{familler_ID.index(ID)}. {ID}")
 
-choose_ID = int(input("Choose the index of the ID you want to select: "))
-selected_ID = familler_ID[choose_ID]
-print(f"You have selected ID: {selected_ID}")
+    choose_ID = int(input("Choose the index of the ID you want to select: "))
+    selected_ID = familler_ID[choose_ID]
+    print(f"You have selected ID: {selected_ID}")
 
-prod = product(selected_ID)
-print(prod.name)   
-print(prod.order(int(input("how many woul you like to order"))))
+    prod = product(selected_ID)
+    print(prod.name)   
+    print(prod.order(int(input("how many woul you like to order"))))
 
-choice = input("would you like to add product ID")
-if choice == "y":
-    ne = input("enter product name")
-    id = input("product_id")
-    pr = int(input("add the products price"))
+    choice = input("would you like to add product ID")
+    if choice == "y":
+        ne = input("enter product name")
+        id = input("product_id")
+        pr = int(input("add the products price"))
 
-    prod.add_product(id, ne, pr)
-    
-    print("succsesful here is the new list of products")
-    print(prod.product_data)
+        prod.add_product(id, ne, pr)
+        
+        print("succsesful here is the new list of products")
+        print(prod.product_data)
