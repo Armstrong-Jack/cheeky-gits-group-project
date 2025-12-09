4643e292878b42728ad71389194c7d79f4ce1905
Jack Armstrong <jackarmstrong819@gmail.com>
2025-12-09 19:27:04 +0000

    Update id_name_search_test.py

--- id_name_search_test.py
+++ id_name_search_test.py
@@ -3,3 +3,19 @@ ids = list(product.keys())
 print(ids)
 
 user_choice = input("how would you like to search. by ID(1) or by Name(2)")
+
+if user_choice == "1":
+    try:
+        entered_id =int(input("please enter the products id")
+        break
+    except ValueError:
+        print("entered wrong value. neededs to be number")
+
+    for id in ids:
+        if id == entered_id:
+            selected_id = id
+            print(f"your selected id is {selected_id})
+
+if user_choice == "2":
+
+        
