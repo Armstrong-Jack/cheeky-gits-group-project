f7c77107aed8d112ea87134e216b3d4b823612fa
Jack Armstrong <jackarmstrong819@gmail.com>
2025-12-09 14:17:07 +0000

    adding full ID search

--- search_test.py
+++ search_test.py
@@ -3,18 +3,22 @@ IDs = ["26628503", "26628504", "26628505", "26628506", "26628507", "26618501", "
 entered_ID = input("Enter the ID to search: ")
 ID_search_length = len(entered_ID)
 familler_ID =[]
+amounts_of_similer_Ids = 0 
 
 for ID in IDs:
     ID2 = ID[0:ID_search_length]
     if entered_ID == ID2:
         familler_ID.append(ID)
-        print(familler_ID)
+        amounts_of_similer_Ids += 1 
 
-for ID in familler_ID:
-    print(f"{familler_ID.index(ID)}. {ID}")
-
-choose_ID = int(input("Choose the index of the ID you want to select: "))
-selected_ID = familler_ID[choose_ID]
-print(f"You have selected ID: {selected_ID}")
+if amounts_of_similer_Ids > 1:
+    for ID in familler_ID:
+        print(f"{familler_ID.index(ID)}. {ID}")
+    choose_ID = int(input("Choose the index of the ID you want to select: "))
+    selected_ID = familler_ID[choose_ID]
+    print(f"You have selected ID: {selected_ID}")
+else:
+    selected_ID = familler_ID
+    print(f"You have selected ID: {selected_ID}")
 
         
