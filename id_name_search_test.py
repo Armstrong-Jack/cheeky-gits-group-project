5ab1bee1c5f5f83c6983885e5b1fc59a5940a368
Jack Armstrong <jackarmstrong819@gmail.com>
2025-12-09 19:16:52 +0000

    Update id_name_search_test.py

--- id_name_search_test.py
+++ id_name_search_test.py
@@ -1,3 +1,5 @@
 product =  {101:  { "Name": "Laptop", "price": 99.99, "quantity": 20}} 
 ids = list(product.keys())
 print(ids)
+
+user_choice = input("how would you like to search. by ID(1) or by Name(2)")
