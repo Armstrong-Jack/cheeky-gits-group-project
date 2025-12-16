073f217c022c42a6a2d09af851ee340e6e0b0266
Jack Armstrong <jackarmstrong819@gmail.com>
2025-12-16 09:52:50 +0000

    Create alan_search.py

---
+++ alan_search.py
@@ -0,0 +1,14 @@
+
+"""this is the ID and product name search task for Alan
+DO NOT REMOVE OR CHANGE the structure for the dictionary below
+your example piece is .py"""
+product =  {
+    101:  {"name": "Laptop", "price": 99.99, "quantity": 20},
+    102: {"name": "PC", "price": 200, "quantity": 10},
+    103: {"name": "keyboard", "price":200 , "quantity": 200},
+    104: {"name": "Mouse" , "price":100 , "quantity":200 },
+    105: {"name": "speakers", "price":100 , "quantity":200 },
+    106: {"name":"HEadphones" , "price":100 , "quantity":200 },
+    107: {"name": "Xbox Controller", "price":100 , "quantity":200 },
+    108: {"name":"PS4 Controller" , "price":100 , "quantity":200 },
+    }
