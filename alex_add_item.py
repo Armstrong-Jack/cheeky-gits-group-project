21064fbee2219b4cd6405f782d9cd48c468c89e6
Jack Armstrong <jackarmstrong819@gmail.com>
2025-12-16 09:49:18 +0000

    Create alex_add_item.py
    adding file

---
+++ alex_add_item.py
@@ -0,0 +1,14 @@
+
+"""this is the add item to the dictionary below task for Alax
+DO NOT REMOVE OR CHANGE the structure for the dictionary below
+your example piece is display_table.py"""
+product =  {
+    101:  {"name": "Laptop", "price": 99.99, "quantity": 20},
+    102: {"name": "PC", "price": 200, "quantity": 10},
+    103: {"name": "keyboard", "price":200 , "quantity": 200},
+    104: {"name": "Mouse" , "price":100 , "quantity":200 },
+    105: {"name": "speakers", "price":100 , "quantity":200 },
+    106: {"name":"HEadphones" , "price":100 , "quantity":200 },
+    107: {"name": "Xbox Controller", "price":100 , "quantity":200 },
+    108: {"name":"PS4 Controller" , "price":100 , "quantity":200 },
+    }
