4aad0d1444fcd3f05bd1c824e58c16aa364ea3d3
jamie <18fouldsj@gmail.com>
2026-01-05 12:23:04 +0000

    f

---
+++ jamie_work.py
@@ -0,0 +1,64 @@
+
+"""this is the json save and load task for Jamie
+DO NOT REMOVE OR CHANGE the structure for the dictionary below
+your example piece is the def load and save functions in the MAIN.py"""
+product =  {
+    101:  {"name": "Laptop", "price": 99.99, "quantity": 20},
+    102: {"name": "PC", "price": 200, "quantity": 10},
+    103: {"name": "keyboard", "price":200 , "quantity": 200},
+    104: {"name": "Mouse" , "price":100 , "quantity":200 },
+    105: {"name": "speakers", "price":100 , "quantity":200 },
+    106: {"name":"HEadphones" , "price":100 , "quantity":200 },
+    107: {"name": "Xbox Controller", "price":100 , "quantity":200 },
+    108: {"name":"PS4 Controller" , "price":100 , "quantity":200 },
+    }
+
+import json
+
+#code imported from main.py for testing
+def add_item(inventory):
+    product = inventory
+    ids = list(product.keys())
+    new_product = {}
+
+
+
+    print("you have selected to add a product")
+            
+    new_product["name"] = input("enter product name")
+
+    while True:
+            try:
+                new_product["price"] = int(input("please enter the product's price"))
+                break
+            except ValueError:
+                print("entered wrong value needs to be numbers")
+            
+    while True:
+        try:
+            new_product["quantity"] = int(input("please enter the product's quantity"))
+            break
+        except ValueError:
+            print("entered wrong value needs to be numbers")
+
+    try:
+        product[ids[len(ids)-1]+1] = new_product
+    except IndexError:
+        product[101] = new_product
+            
+    print(new_product)
+    print(product)
+    return(product)
+
+def load_data():
+    try:
+        with open("inventory.json", "r") as f:
+            inventory_data = json.load(f)
+    except FileNotFoundError:
+        print("Error: File Not found\nCreating new file\nFile needs an item for the file, please create one")
+        with open("inventory.json", "w") as f:
+            inventory = {}
+            inventory_data= add_item(inventory)
+            json.dump(inventory_data, f, indent= 4)
+            print(inventory_data)
+
